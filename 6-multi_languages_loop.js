0e1a8dbb4046e658972809628a3a05b43c3a4e27
Prichard-1 <genarodube8@gmail.com>
2025-05-24 23:29:05 +0200

    add 6-multi_languages_loop .js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,7 @@
-console.log("C is fun",
-    "Python is cool",
-    "JavaScrript is amazing"
-);
+#!/usr/bin/node
+
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const lang of languages) {
+  console.log(lang);
+}
